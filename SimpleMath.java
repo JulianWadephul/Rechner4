bef5a849980a89a43a40602854d95af410903e7b
julianprog3 <julian.wadephul@gmail.com>
2017-01-24 15:21:40 +0100

    SimpleMath

---
+++ SimpleMath.java
@@ -0,0 +1,23 @@
+
+public class SimpleMath {
+	public static double multiply(double a, double b)
+	{
+		return a * b;
+	}
+
+	public static double divide(double a, double b)
+	{
+		return a / b;
+		
+	}
+	public static double add(double a, double b)
+	{
+		return a + b;
+		
+	}
+	public static double substract(double a, double b)
+	{
+		return a - b;
+		
+	}
+}
